acdc89b89c24e676a0f32ad9065dfe862443ec55
Bryan Marques <bryantlanta@gmail.com>
2026-09-15 00:39:38 -0300

    Implementacao de remover da arvore binaria

--- ArvoreBinaria.c
+++ ArvoreBinaria.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 typedef struct no {
     int val;
@@ -8,15 +9,31 @@ typedef struct no {
 } NO;
 
 NO *criarARV() {
-    NO *novo = malloc(sizeof(NO));
-    if(novo == NULL) {
-        printf("Erro de alocação\n");
+    return NULL;
+}
+
+NO *menor(NO *raiz) {
+    if(raiz == NULL) {
         return NULL;
     }
-    novo->esq = NULL;
-    novo->dir = NULL;
 
-    return NULL;
+    if(raiz->esq == NULL) {
+        return raiz;    
+    }
+
+    return menor(raiz->esq);
+}
+
+NO *maior(NO *raiz) {
+    if(raiz == NULL) {
+        return NULL;
+    }
+
+    if(raiz->dir == NULL) {
+        return raiz;    
+    }
+
+    return maior(raiz->dir);
 }
 
 void inserir(NO **raiz, int valor) {
@@ -33,22 +50,110 @@ void inserir(NO **raiz, int valor) {
     }
 }
 
+void remover (NO **raiz, int valor) {
+
+    if(*raiz == NULL) {
+        printf("A árvore não existe!\n");
+        return;
+    }
+
+    // Caso 1 (nó folha):
+    if(valor < (*raiz)->val) {
+        remover(&(*raiz)->esq, valor);
+    } else if (valor > (*raiz)->val) {
+        remover(&(*raiz)->dir, valor);
+    } else {
+        if((*raiz)->esq == NULL && (*raiz)->dir == NULL) {
+            free(*raiz);
+            *raiz = NULL;
+            printf("Nó folha removido\n");
+        }
+
+        // Caso 2 (nó pai tem 1 filho):
+        else if((*raiz)->esq == NULL) {                //verificando o lado esquerdo
+            NO* aux = *raiz;
+            *raiz = (*raiz)->dir;
+            free(aux);
+            printf("Nó com 1 filho (dir) removido\n");
+        }
+
+        else if((*raiz)->dir == NULL) {                //verificando o lado direita
+            NO* aux = *raiz;
+            *raiz = (*raiz)->esq;
+            free(aux);
+            printf("Nó com 1 filho (esq) removido\n");
+        }
+
+        else {
+            NO *aux = menor((*raiz)->dir);             // escolhi receber o menor número do lado direito
+            (*raiz)->val = aux->val;
+            printf("Caso 3 de remoção executado\n");
+            remover(&(*raiz)->dir, aux->val);
+        }
+
+    }
+}
+
+
 void mostrar(NO **raiz) {
     if(*raiz == NULL) {
         return;
     }
     mostrar(&(*raiz)->esq);
-    printf("%d\n", (*raiz)->val);
+    printf("%d, ", (*raiz)->val);
     mostrar(&(*raiz)->dir);
 }
 
-int main() {
+int main(void) {
+    setlocale(LC_ALL, "Portuguese");
+
     NO* arv = criarARV();
+
+    printf("Inserindo... ");
     inserir(&arv, 10);
     inserir(&arv, 20);
     inserir(&arv, 30);
-    
+    inserir(&arv, 5);
+    inserir(&arv, 7);
     mostrar(&arv);
+    printf("\n");
+
+    remover(&arv, 30);
+
+    printf("Valores: ");
+    mostrar(&arv);
+    printf("\n");
+
+    printf("Inserindo... ");
+    inserir(&arv, 30);
+    mostrar(&arv);
+    printf("\n");
+
+    remover(&arv, 20);
+
+    printf("Valores: ");
+    mostrar(&arv);
+    printf("\n");
+
+    remover(&arv, 5);
+
+    printf("Valores: ");
+    mostrar(&arv);
+    printf("\n");
+
+    printf("Inserindo... ");
+    inserir(&arv, 20);
+    inserir(&arv, 25);
+    inserir(&arv, 15);
+    inserir(&arv, 5);
+    mostrar(&arv);
+    printf("\n");
+
+    remover(&arv, 20);
+
+    printf("Valores: ");
+    mostrar(&arv);
+    printf("\n");
 
     return 0;
 }
